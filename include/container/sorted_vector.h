ca65a15032dc35ef29d3dcee8628aa685cb86fca
Stian Lode <stian.lode@canrig.com>
2019-05-13 16:33:15 +0200

    Added iterators to sortedvector

--- include/container/sorted_vector.h
+++ include/container/sorted_vector.h
@@ -12,50 +12,144 @@ template <typename T>
 class SortedVector
 {
 public:
+
+  class TypeWrapper;
+  class SortedVectorIterator;
+  class CSortedVectorIterator;
+
   typedef T value_type;
+  typedef T* pointer;
+  typedef T& reference;
+  typedef size_t size_type;
+  typedef std::vector<TypeWrapper> ValueVector;
+  typedef SortedVectorIterator iterator;
+  typedef CSortedVectorIterator const_iterator;
 
   struct TypeWrapper {
-    size_t index;
-    T value;
-    bool operator<(const size_t &other) const
+    size_type id;
+    value_type value;
+    bool operator<(const size_type &other) const
     {
-      return index < other;
+      return id < other;
     }
   };
 
-  SortedVector()
-    :buffer(){};
+  class SortedVectorIterator {
+    SortedVector &m_sortedVector;
+    size_type m_index;
+
+  public:
+    SortedVectorIterator(SortedVector &svec, size_type index)
+      : m_sortedVector(svec), m_index(index) {}
+
+    reference operator*() {
+      return m_sortedVector.m_buffer[m_index].value;
+    }
+
+    iterator& operator++() {
+      ++m_index;
+      return *this;
+    }
+
+    iterator operator++(int) {
+      iterator clone(*this);
+      ++m_index;
+      return clone;
+    }
+    
+    bool operator==(const iterator &other) {
+      return &m_sortedVector == &other.m_sortedVector 
+             && m_index == other.m_index;
+    }
+    
+    bool operator!=(const iterator &other) {
+      return !(*this == other);
+    }
+  };
+
+  class CSortedVectorIterator {
+    SortedVector &m_sortedVector;
+    size_type m_index;
+
+  public:
+    CSortedVectorIterator(SortedVector &svec, size_type index)
+      : m_sortedVector(svec), m_index(index) {}
+
+    const reference operator*() const {
+      return m_sortedVector.m_buffer[m_index].value;
+    }
+
+    const_iterator& operator++() {
+      ++m_index;
+      return *this;
+    }
+
+    const_iterator operator++(int) {
+      const_iterator clone(*this);
+      ++m_index;
+      return clone;
+    }
+    
+    bool operator==(const const_iterator &other) const {
+      return m_sortedVector == other.m_sortedVector 
+             && m_index == other.m_index;
+    }
+    
+    bool operator!=(const const_iterator &other) const {
+      return !(*this == other);
+    }
+  };
+
+  SortedVector() :m_buffer(){};
   ~SortedVector() {};
 
-  void insert(size_t index, const T &value) {
-    size_t found = binary_search(buffer, index);
-    TypeWrapper wrap{index, value};
-    buffer.insert(buffer.begin() + found, wrap);
+  iterator begin() {
+    return iterator(*this, 0);
+  }
+
+  iterator end() {
+    return iterator(*this, m_buffer.size());
+  }
+
+  const_iterator begin() const {
+    return const_iterator(*this, 0);
+  }
+
+  const_iterator end() const {
+    return const_iterator(*this, m_buffer.size());
+  }
+
+  void insert(size_t id, const T &value) {
+    size_t found = binary_search(m_buffer, id);
+    TypeWrapper wrap{id, value};
+    m_buffer.insert(m_buffer.begin() + found, wrap);
   }
   
-  T& at(size_t index) {
-    size_t found = binary_search(buffer, index);
-    TypeWrapper& result = buffer.at(found);
-    if (result.index != index) {
-      throw std::out_of_range("index not found.");
+  T& at(size_t id) {
+    size_t found = binary_search(m_buffer, id);
+    TypeWrapper& result = m_buffer.at(found);
+    if (result.id != id) {
+      throw std::out_of_range("");
     }
     return result.value;
   }
 
-  void erase(size_t index) {
-    size_t found = binary_search(buffer, index);
-    TypeWrapper& result = buffer.at(found);
-    if (result.index == index) {
-      buffer.erase(buffer.begin() + found);
+  void erase(size_t id) {
+    size_t found = binary_search(m_buffer, id);
+    TypeWrapper& result = m_buffer.at(found);
+    if (result.id == id) {
+      m_buffer.erase(m_buffer.begin() + found);
     }
   }
 
   size_t size() {
-    return buffer.size();
+    return m_buffer.size();
   }
 
+
+
 private:
-  std::vector<TypeWrapper> buffer;
+  ValueVector m_buffer;
 };
 
 }}
--- tests/test_sorted_vector.cpp
+++ tests/test_sorted_vector.cpp
@@ -13,7 +13,6 @@ struct Position {
 SHL_TEST_SUITE(SortedVector)
 
 SHL_TEST_CASE(Insert) {
-
   SortedVector<Position> vec;
   vec.insert(0, Position{0.0, 3.0});
   vec.insert(1, Position{1.0, 1.0});
@@ -22,7 +21,27 @@ SHL_TEST_CASE(Insert) {
   vec.insert(4, Position{4.0, 1.0});
   vec.insert(5, Position{5.0, 1.0});
   vec.insert(6, Position{6.0, 1.0});
+  vec.insert(7, Position{7.0, 1.0});
+  vec.insert(8, Position{8.0, 1.0});
+
+  SHL_TEST_EQUAL(vec.at(7).x, 7.0);
 }
+
+SHL_TEST_CASE(InsertReverse) {
+  SortedVector<Position> vec;
+  vec.insert(8, Position{8.0, 1.0});
+  vec.insert(7, Position{7.0, 1.0});
+  vec.insert(6, Position{6.0, 1.0});
+  vec.insert(5, Position{5.0, 1.0});
+  vec.insert(4, Position{4.0, 1.0});
+  vec.insert(3, Position{3.0, 1.0});
+  vec.insert(2, Position{2.0, 1.0});
+  vec.insert(1, Position{1.0, 1.0});
+  vec.insert(0, Position{0.0, 3.0});
+
+  SHL_TEST_EQUAL(vec.at(7).x, 7.0);
+}
+
 SHL_TEST_CASE(Get) {
   SortedVector<Position> vec;
   vec.insert(7, Position{7.0, 1.0});
@@ -48,5 +67,27 @@ SHL_TEST_CASE(Delete) {
   SHL_TEST_EQUAL(vec.size(), 2);
   vec.erase(7);
   SHL_TEST_EQUAL(vec.size(), 1);
+}
+
+SHL_TEST_CASE(Iterator) {
+  SortedVector<Position> vec;
+  vec.insert(7, Position{7.0, 1.0});
+  vec.insert(8, Position{8.0, 1.0});
+
+  auto it = vec.begin();
+
+  SHL_TEST_EQUAL((*it++).x, 7.0);
+  SHL_TEST_EQUAL((*it++).x, 8.0);
+  SHL_TEST_TRUE(it == vec.end());
+}
+
+SHL_TEST_CASE(IteratorLoop) {
+  SortedVector<Position> vec;
+  vec.insert(7, Position{7.0, 1.0});
+  vec.insert(8, Position{8.0, 1.0});
+
+  for (auto p : vec) {
+    SHL_TEST_TRUE(p.x > 0);
+  }
 }
 SHL_TEST_SUITE_END
