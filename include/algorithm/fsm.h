227aa62ed01708d4e552457162eab9b9b042bb09
Stian Lode <stian.lode@canrig.com>
2020-05-25 15:08:32 +0200

    Adds fsm

---
+++ include/algorithm/fsm.h
@@ -0,0 +1,93 @@
+#include <map>
+#include <queue>
+
+namespace shl {
+namespace algorithm {
+
+class Machine;
+
+class EventBase {
+public:
+  virtual ~EventBase() {}
+  EventBase(EventBase const&) = delete;
+  virtual void operator=(EventBase const&) = delete;
+protected:
+  EventBase() {}
+};
+
+template <typename T>
+class Event: public EventBase {
+public:
+  static T* id() {
+    static T e;
+    return &e;
+  }
+};
+
+class StateBase {
+public:
+  virtual void enter(Machine *fsm){};
+  virtual void exit(Machine *fsm){};
+  virtual void update(Machine *fsm) = 0;
+  virtual ~StateBase() {}
+
+  StateBase(StateBase const&) = delete;
+  virtual void operator=(StateBase const&) = delete;
+protected:
+  StateBase() {}
+};
+
+template <typename T>
+class State: public StateBase {
+public:
+  static T* id() {
+    static T s;
+    return &s;
+  }
+};
+
+class Machine {
+  typedef std::queue<const EventBase*> EventQueue;
+  typedef std::pair<const StateBase*, const EventBase*> EventPair;
+  typedef std::map<const EventPair, StateBase*> TransitionMap;
+
+  StateBase *currentState{0};
+  EventQueue eventQueue;
+protected:
+  TransitionMap transitions;
+
+  virtual void setState(StateBase *state) {
+    if (currentState) currentState->exit(this);
+    currentState = state;
+    if (currentState) currentState->enter(this);
+  }
+
+public:
+  virtual ~Machine() {}
+
+  virtual void signal(EventBase *event) {
+    eventQueue.push(event);
+  }
+
+  virtual void defineTransition(const StateBase *from, const EventBase *event, StateBase *to) {
+    transitions[std::make_pair(from, event)] = to;
+  }
+
+  virtual void update() {
+    if (currentState) currentState->update(this);
+
+    if (!eventQueue.empty()) {
+      try {
+        setState(transitions.at(std::make_pair(currentState, eventQueue.front())));
+      } catch (const std::out_of_range &e) {
+        try {
+          setState(transitions.at(std::make_pair(currentState, nullptr)));
+        } catch (const std::out_of_range &e) {
+          setState(transitions.at(std::make_pair(nullptr, nullptr)));
+        }
+      }
+      eventQueue.pop();
+    }
+  }
+};
+}}
---
+++ tests/test_fsm.cpp
@@ -0,0 +1,71 @@
+#include "test/test.h"
+#include "algorithm/fsm.h"
+
+using namespace shl::test;
+using namespace shl::algorithm;
+
+#define TRACE \
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+class TurnOn: public Event<TurnOn> {};
+class TurnOff: public Event<TurnOff> {};
+
+class Off: public State<Off> {
+public:
+  void update(Machine* m) {
+    TRACE
+    m->signal(TurnOn::id());
+  }
+};
+
+class On: public State <On>{
+  int counter{0};
+public:
+  void enter(Machine *m) {
+  }
+  void update(Machine* m) {
+    TRACE
+    if (counter++ > 5)
+      m->signal(TurnOn::id());
+    if (counter++ > 2)
+      m->signal(TurnOff::id());
+  }
+};
+
+class GlobalDefault: public State <GlobalDefault>{
+public:
+  void update(Machine* m) {
+    TRACE
+    m->signal(TurnOff::id());
+  }
+};
+
+class OnDefault: public State <OnDefault>{
+public:
+  void update(Machine* m) {
+    TRACE
+    m->signal(TurnOff::id());
+  }
+};
+
+class JSM: public Machine {
+  public:
+  JSM() {
+   defineTransition(0, 0, GlobalDefault::id());
+   defineTransition(GlobalDefault::id(), 0, Off::id());
+   defineTransition(On::id(), TurnOff::id(), Off::id());
+   defineTransition(On::id(), 0, OnDefault::id());
+   defineTransition(Off::id(), TurnOn::id(), On::id());
+   signal(0);
+  }
+};
+
+SHL_TEST_SUITE(FSM)
+
+SHL_TEST_CASE(Initialization) {
+  JSM mach;
+  for (size_t i = 0; i<10; i++)
+    mach.update();
+}
+
+SHL_TEST_SUITE_END
