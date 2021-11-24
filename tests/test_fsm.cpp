bc285e4ff6125e45586b94e68bab29ee9b3149b0
Stian Lode <stian.lode@canrig.com>
2021-11-24 13:01:20 +0100

    Cleanup

--- tests/test_fsm.cpp
+++ tests/test_fsm.cpp
@@ -7,69 +7,128 @@ using namespace shl::algorithm;
 #define TRACE \
     std::cout << __PRETTY_FUNCTION__ << std::endl;
 
-class TurnOn: public Event<TurnOn> {};
-class TurnOff: public Event<TurnOff> {};
-
-class Off: public State<Off> {
+/** DAYLIGHT DEF **/
+class Daylight: public Machine {
+  uint hour_;
 public:
-  void update(Machine* m) {
-    TRACE
-    m->signal(TurnOn::id());
+  void incrementHour() {
+    hour_++;
+    hour_ %= 24;
+  }
+  uint getHour() const {
+    return hour_;
+  }
+  bool isDay() const {
+    return hour_ > 7 && hour_ < 19;
   }
 };
 
-class On: public State <On>{
+class SunriseEvent: public Event<SunriseEvent> {};
+class SunsetEvent: public Event<SunsetEvent> {};
+
+class DayState: public State<DayState> {
+public:
+  void enter(Machine *m) {
+    TRACE
+    Daylight *day = static_cast<Daylight*>(m);
+    std::cout << day->getHour() << std::endl;
+  }
+  void update(Machine* m) {
+    Daylight *day = static_cast<Daylight*>(m);
+    day->incrementHour();
+    if (!day->isDay()) day->signal(SunsetEvent::id());
+  }
+  void exit(Machine *m) {
+    TRACE
+    Daylight *day = static_cast<Daylight*>(m);
+    std::cout << day->getHour() << std::endl;
+  }
+};
+
+class NightState: public State <NightState>{
   int counter{0};
+public:
+  void enter(Machine *m) {
+    TRACE
+    Daylight *day = static_cast<Daylight*>(m);
+    std::cout << day->getHour() << std::endl;
+  }
+  void update(Machine* m) {
+    Daylight *day = static_cast<Daylight*>(m);
+    day->incrementHour();
+    if (day->isDay()) day->signal(SunriseEvent::id());
+  }
+  void exit(Machine *m) {
+    TRACE
+    Daylight *day = static_cast<Daylight*>(m);
+    std::cout << day->getHour() << std::endl;
+  }
+};
+
+/** LAMP DEF **/
+class LampOnEvent: public Event<LampOnEvent> {};
+class LampOffEvent: public Event<LampOffEvent> {};
+
+class Lamp: public Machine {
+  int hours_on = 0;
+  Daylight &day_;
+public:
+  Lamp(Daylight &day): day_(day)  {}
+  const Daylight& getDaylight() { return day_; }
+};
+
+class LampOffState: public State<LampOffState> {
 public:
   void enter(Machine *m) {
     TRACE
   }
   void update(Machine* m) {
-    TRACE
-    if (counter++ > 5)
-      m->signal(TurnOn::id());
-    if (counter++ > 2)
-      m->signal(TurnOff::id());
+    Lamp *lamp = static_cast<Lamp*>(m);
+    if (lamp->getDaylight().isDay()) {
+      lamp->signal(LampOnEvent::id());
+    }
   }
   void exit(Machine *m) {
     TRACE
   }
 };
 
-class GlobalDefault: public State <GlobalDefault>{
+class LampOnState: public State <LampOnState>{
+  int counter{0};
 public:
-  void update(Machine* m) {
+  void enter(Machine *m) {
     TRACE
-    m->signal(TurnOff::id());
   }
-};
-
-class OnDefault: public State <OnDefault>{
-public:
   void update(Machine* m) {
-    TRACE
-    m->signal(TurnOff::id());
+    Lamp *lamp = static_cast<Lamp*>(m);
+    if (!lamp->getDaylight().isDay()) {
+      lamp->signal(LampOffEvent::id());
+    }
   }
-};
-
-class JSM: public Machine {
-  public:
-  JSM() {
-   defineTransition(0, 0, GlobalDefault::id());
-   defineTransition(GlobalDefault::id(), 0, Off::id());
-   defineTransition(On::id(), TurnOff::id(), Off::id());
-   defineTransition(On::id(), 0, OnDefault::id());
-   defineTransition(Off::id(), TurnOn::id(), On::id());
-   signal(0);
+  void exit(Machine *m) {
+    TRACE
   }
 };
 
 SHL_TEST_SUITE(FSM)
 
 SHL_TEST_CASE(Initialization) {
-  JSM mach;
-  for (size_t i = 0; i<10; i++)
-    mach.update();
+  Daylight day;
+  day.defineTransition(0,                0,                  NightState::id());
+  day.defineTransition(NightState::id(), SunriseEvent::id(), DayState::id());
+  day.defineTransition(DayState::id(),   SunsetEvent::id(),  NightState::id());
+  day.signal(0);
+
+  Lamp lamp(day);
+  lamp.defineTransition(0,                  0,                  LampOffState::id());
+  lamp.defineTransition(LampOffState::id(), LampOnEvent::id(),  LampOnState::id());
+  lamp.defineTransition(LampOnState::id(),  LampOffEvent::id(), LampOffState::id());
+  lamp.signal(0);
+
+  for (int i = 0; i<48; i++) {
+    day.update();
+    lamp.update();
+  }
 }
 
 SHL_TEST_SUITE_END
